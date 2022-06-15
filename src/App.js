ab148f881e75c337b6a3852fe77bd258170d3caf
gerakanamalindonesia <gerakanamalindonesia@gmail.com>
2022-06-15 15:46:21 +0700

    syar'i with me updated

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Syar'i With Me (By Tante Ani :D)</h1>
+      <h1>Syar'i With Me (Availabel Soon)</h1>
     </div>
   );
 }
